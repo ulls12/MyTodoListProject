e04c5efb4b5a7818b8941a4364770e953ea0c736
ulls12 <koes823@gmail.com>
2023-12-20 19:00:54 +0900

    Initial Commit

---
+++ MyTodoListProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTodoListProject
+//
+//  Created by t2023-m0044 on 12/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
